ba71da97e3bc44f9ccaa74d95ea061b453ac0c8a
unknown <adeelnasirkbw@gmail.com>
2022-08-25 02:22:13 -0700

    pages and there exports

---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+export { default as Calender } from './Calender';
+export { default as ColorPicker } from './ColorPicker';
+export { default as Customer } from './Customer';
+export { default as Employees } from './Employees';
+export { default as Ecommerce } from './Ecommerce';
+export { default as Editor } from './Editor';
+export { default as Kanban } from './Kanban';
+export { default as Order } from './Order';
+
+export { default as Area } from './charts/Area';
+export { default as Bar } from './charts/Bar';
+export { default as ColorMapping } from './charts/ColorMapping';
+export { default as Financial } from './charts/Financial';
+export { default as LineChart } from './charts/LineChart';
+export { default as Pie } from './charts/Pie';
+export { default as Pyramid } from './charts/Pyramid';
+export { default as Stacked } from './charts/Stacked';
